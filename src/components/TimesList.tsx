54ddbcc152c19c29bac53c7864efa14add90880f
Matthew Canabarro <67874965+mattc50@users.noreply.github.com>
2024-12-30 11:11:26 -0500

    made date headers sticky on time list

--- src/components/TimesList.tsx
+++ src/components/TimesList.tsx
@@ -1,7 +1,5 @@
 import { Animated, Pressable, ScrollView, StyleSheet, Text, View } from "react-native"
-import TextField from "./TextField";
 import { useEffect, useRef, useState } from "react";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import ModalScreen from "./ModalScreen";
@@ -25,8 +23,6 @@ const TimesList: React.FC<TimesListProps> = ({ data, setData, showList, setShowL
 
   const [dates, setDates] = useState<string[]>([])
 
-  // const [loading, setLoading] = useState<boolean>(true);
-
   const closeList = () => {
     Animated.timing(listOpacity, {
       toValue: 0,
@@ -47,18 +43,17 @@ const TimesList: React.FC<TimesListProps> = ({ data, setData, showList, setShowL
     }
   }
 
-  const removeData = async (key: any) => {
-    try {
-      await AsyncStorage.removeItem(key);
-      return true;
-    }
-    catch (exception) {
-      return false;
-    }
-  }
+  // const removeData = async (key: any) => {
+  //   try {
+  //     await AsyncStorage.removeItem(key);
+  //     return true;
+  //   }
+  //   catch (exception) {
+  //     return false;
+  //   }
+  // }
 
   const removeItem = async (date: string, index: number) => {
-    // setLoading(true);
     const dataObj: any = { ...data }
     const dateArray = dataObj[date];
     const modifiedArray = dateArray;
@@ -72,7 +67,6 @@ const TimesList: React.FC<TimesListProps> = ({ data, setData, showList, setShowL
     setDates(Object.keys(modifiedData))
 
     await storeData(modifiedData);
-    // setLoading(false);
   }
 
   const renderDate = (date: string) => {
@@ -88,7 +82,6 @@ const TimesList: React.FC<TimesListProps> = ({ data, setData, showList, setShowL
       const dataJSON = { ...data };
       setDates(Object.keys(dataJSON));
     }
-    // setLoading(false)
   }, [data])
 
   return (
@@ -113,18 +106,21 @@ const TimesList: React.FC<TimesListProps> = ({ data, setData, showList, setShowL
         <ScrollView
           contentContainerStyle={{ paddingHorizontal: 16 }}
           keyboardShouldPersistTaps='handled'
+          stickyHeaderIndices={dates.map((_, index) => index * 2)}
         >
           {dates.length > 0 && dates.map((date, index) => {
             const dataIndex: any = data[date as keyof DataObject];
 
-            return (
-              <View key={index}>
+            return [
+              <View style={styles.stickyHeader}>
                 <Text style={styles.date}>
                   {renderDate(date)}
                 </Text>
+              </View>,
+              <View style={{ marginBottom: index + 1 === dates.length ? 0 : 16 }}>
                 {dataIndex.map((item: string[], index: number) => {
                   return (
-                    <View key={index} style={styles.taskItem}>
+                    <View key={index} style={[styles.taskItem, { marginBottom: index + 1 === dataIndex.length ? 0 : 8 }]}>
                       <Text style={[styles.text, styles.name]}>{Object.keys(item)}</Text>
                       <Text style={[styles.text, styles.time]}>{formatTime(Object.values(item))}</Text>
                       <Pressable
@@ -136,9 +132,8 @@ const TimesList: React.FC<TimesListProps> = ({ data, setData, showList, setShowL
                     </View>
                   )
                 })}
-
               </View>
-            )
+            ]
           })}
           {dates.length === 0 &&
             <View style={{
@@ -157,6 +152,10 @@ const TimesList: React.FC<TimesListProps> = ({ data, setData, showList, setShowL
 }
 
 const styles = StyleSheet.create({
+  stickyHeader: {
+    backgroundColor: "rgb(230, 237, 246)",
+    paddingVertical: 2
+  },
   date: {
     color: "rgba(55, 58, 63, 0.5)",
     fontSize: 16,
@@ -168,7 +167,6 @@ const styles = StyleSheet.create({
   taskItem: {
     display: "flex",
     flexDirection: "row",
-    marginBottom: 8
   },
   name: {
     display: "flex",
